53785acaed5f578aa452053252a0f636a55afd03
David <daveshuang@gmail.com>
2019-10-04 16:39:22 -0700

    added footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,26 @@ import '../style/Footer.css';
 export default function Footer() {
   return (
     <div>
-      <div className='footer'>LoX Entertainment</div>
+      <div className='footer'>
+        <span>L</span>
+        <span>o</span>
+        <span>X</span>
+        <span>&nbsp;</span>
+        <span>E</span>
+        <span>n</span>
+        <span>t</span>
+        <span>e</span>
+        <span>r</span>
+        <span>t</span>
+        <span>a</span>
+        <span>i</span>
+        <span>n</span>
+        <span>m</span>
+        <span>e</span>
+        <span>n</span>
+        <span>t</span>
+        <span>&copy;</span>
+      </div>
     </div>
   );
 }
