e3c9b99efe9d29026a18df1aba5e996114c0f342
David <daveshuang@gmail.com>
2019-10-03 20:05:46 -0700

    partial fix of game lobby display

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,31 +3,24 @@ import Chat from './socketComponents/Chat';
 import GameMode from './socketComponents/GameMode';
 
 class Dashboard extends Component {
-
   constructor(props) {
     super(props);
-
   }
 
   render() {
     return (
       <div>
-        <div className="headerpage jumbotron">
-          <div className="container">
-            <h1 className="display-3">Welcome back,{' ' + this.props.username}</h1>
-            <b>
-              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-              ea commodo consequat. Duis aute irure dolor in reprehenderitin voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </b>
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <h1 className='display-3'>
+                Welcome back,{' ' + this.props.username}
+              </h1>
+            </div>
           </div>
         </div>
-        <div className="row 2">
-          <Chat username={this.props.username} />
-          <GameMode />
-        </div>
+        <Chat username={this.props.username} />
+        <GameMode />
       </div>
     );
   }
--- client/src/components/socketComponents/GameMode.js
+++ client/src/components/socketComponents/GameMode.js
@@ -12,30 +12,24 @@ class GameMode extends Component {
 
   render() {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col s12 m7'>
-            <div className='card gamemode'>
-              <div className='header'>
-                <h1 className='card-title center'>REGULAR GAME</h1>
-              </div>
+      <div className='row'>
+        <div className='col s12 m7'>
+          <div className='card gamemode'>
+            <div className='header'>
+              <h1 className='card-title center'>REGULAR GAME</h1>
+            </div>
 
-              <div className='card-content'>
-                <p>
-                  Are you ready to play a round of Multiplayer Quiz? All you
-                  need to do is click the Start button and you are able to play
-                  against other players on Quizznet! What are you waiting for?
-                </p>
-              </div>
+            <div className='card-content'>
+              <p>Are you ready to play a round of Multiplayer Quiz?</p>
+            </div>
 
-              <div className='card-action'>
-                <button
-                  className='btn btn-primary form-control'
-                  onClick={this.start.bind(this)}
-                >
-                  Start Game
-                </button>
-              </div>
+            <div className='card-action'>
+              <button
+                className='btn btn-primary form-control'
+                onClick={this.start.bind(this)}
+              >
+                Start Game
+              </button>
             </div>
           </div>
         </div>
