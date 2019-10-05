d89bcd44a7d807510467a126a1e909e7cb216e74
Andy <andymendez100@gmail.com>
2019-10-04 17:37:39 -0700

    change button sizing on main page to even them out and made timer 15 seconds

--- client/src/components/socketComponents/Timer.js
+++ client/src/components/socketComponents/Timer.js
@@ -1,69 +1,69 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
 
 class Timer extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             CountDown: 1,
-            QuestionNumber: 0, 
+            QuestionNumber: 0,
             gameIsOver: false
         };
 
-        if(process.env.NODE_ENV === 'production'){
+        if (process.env.NODE_ENV === 'production') {
             this.socket = io('/');
-        }else if(process.env.NODE_ENV === 'development'){
-            this.socket = io('localhost:3000'); 
+        } else if (process.env.NODE_ENV === 'development') {
+            this.socket = io('localhost:3000');
         }
     }
 
     Timer() {
         this.setState({
-          CountDown: this.state.CountDown - 1
-        }); 
+            CountDown: this.state.CountDown - 1
+        });
         this.props.data(this.state.CountDown);
-       
 
-        if(this.state.CountDown < 1) { 
-          clearInterval(this.interval);
+
+        if (this.state.CountDown < 1) {
+            clearInterval(this.interval);
         }
-        
-        if(this.state.QuestionNumber < 10){
-            if(this.state.CountDown === 0){
+
+        if (this.state.QuestionNumber < 10) {
+            if (this.state.CountDown === 0) {
                 this.setState({
-                    CountDown: 10
+                    CountDown: 15
                 });
-                
-                this.setState({QuestionNumber: this.state.QuestionNumber + 1});
+
+                this.setState({ QuestionNumber: this.state.QuestionNumber + 1 });
                 clearInterval(this.interval);
-                this.socket.emit('quiz', "new_question"); 
+                this.socket.emit('quiz', "new_question");
                 this.interval = setInterval(this.Timer.bind(this), 1000);
             }
-        }else if(this.state.QuestionNumber === 10 && this.state.gameIsOver === false){
+        } else if (this.state.QuestionNumber === 10 && this.state.gameIsOver === false) {
             this.props.number(this.state.QuestionNumber);
-            this.setState({gameIsOver: true});
+            this.setState({ gameIsOver: true });
             this.props.score();
         }
 
-        if(this.state.QuestionNumber === 1){
+        if (this.state.QuestionNumber === 1) {
             let value = {
                 MatchIdentication: this.props.matchtoken
             };
             axios.post('/api/endgame', value);
         }
     }
-    
-      componentDidMount() {
+
+    componentDidMount() {
         this.interval = setInterval(this.Timer.bind(this), 1000);
     }
-      componentWillUnmount(){
+    componentWillUnmount() {
         clearInterval(this.interval);
     }
-      
+
     render() {
-       
-        return(
+
+        return (
             <div className="container">
                 <h5 className="center">{`Question: ${this.state.QuestionNumber}/10`}</h5>
                 <h5 className="center">{`Timer: ${this.state.CountDown}`}</h5>
